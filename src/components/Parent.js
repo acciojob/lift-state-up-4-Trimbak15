920e65916543d3e04b0aaedae07f4a643d27cd0a
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-23 10:24:56 +0000

    mere bhaii q ni hora hai sab toh sahi kr diya ab toh hoja

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -84,7 +84,6 @@ const Parent = () => {
     { id: 1, name: "Item 1", price: 10 },
     { id: 2, name: "Item 2", price: 20 },
     { id: 3, name: "Item 3", price: 30 },
-    { id: 4, name: "Item 4", price: 40 }
   ]);
 
      const [itemName, setItemName] = useState("");
@@ -148,7 +147,7 @@ const Child = ({ cartItems, handleRemoveItem }) => {
         <div key={item.id} className="item">
           <ul>
             <li>
-            <span id="itemName">{item.name}-</span>
+            <span id="itemName">{item.name} - </span>
           <span id="itemPrice">${item.price}</span>
           <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
             </li>
